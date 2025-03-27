58802b79761ebcdfda7b5baca6ef41447c381046
Erica <you@example.com>
2025-03-27 13:53:38 +0100

    feat: testAddVehicle done. TDD: ❌

--- src/test/java/dev/erika/toll_station/tollstation/model/TollStationTest.java
+++ src/test/java/dev/erika/toll_station/tollstation/model/TollStationTest.java
@@ -1,9 +1,13 @@
 package dev.erika.toll_station.tollstation.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import dev.erika.toll_station.vehicles.model.Car;
+import dev.erika.toll_station.vehicles.model.Motorbike;
 
 public class TollStationTest {
     @Test
@@ -19,5 +23,14 @@ public class TollStationTest {
     public void testVehicleList() {
         TollStation tollStation = new TollStation("Autogrill", "Sagunto");
         assertEquals(0, tollStation.getVehicles().size());
+    }
+
+    @Test
+    @DisplayName("Test para comprobar que se añade un coche a la lista")
+    public void testAddVehicle() {
+        TollStation tollStation = new TollStation("Autogrill", "Sagunto");
+        Car car = new Car("1234ABC");
+        tollStation.addVehicle(car);
+        assertTrue(tollStation.getVehicles().contains(car));
     }
 }
