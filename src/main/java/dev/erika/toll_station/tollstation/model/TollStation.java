2237e21323cb5d18426dd98a7e3597726f84b7ef
Erica <you@example.com>
2025-03-27 13:41:01 +0100

    feat: Vehicles list implemented. TDD: ✔️

--- src/main/java/dev/erika/toll_station/tollstation/model/TollStation.java
+++ src/main/java/dev/erika/toll_station/tollstation/model/TollStation.java
@@ -1,9 +1,15 @@
 package dev.erika.toll_station.tollstation.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.erika.toll_station.vehicles.model.Vehicle;
+
 public class TollStation {
 
     private final String name;
     private final String city;
+    private final List<Vehicle> vehicles = new ArrayList<>();
 
     public TollStation(String name, String city) {
         this.name = name;
@@ -16,5 +22,9 @@ public class TollStation {
 
     public String getCity() {
         return city;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
     }
 }
