6efcc5ddba50d90f2dd6ca769fac3345039c9e0a
Erica <you@example.com>
2025-03-27 13:00:43 +0100

    feat: Car class created and implemented.  TDD: ✔️

---
+++ src/main/java/dev/erika/toll_station/vehicles/model/Car.java
@@ -0,0 +1,12 @@
+package dev.erika.toll_station.vehicles.model;
+
+public class Car extends Vehicle {
+    public Car(String plate) {
+        super(plate);
+    }
+
+    @Override
+    public int calculateToll() {
+        return 50;
+    }
+}
