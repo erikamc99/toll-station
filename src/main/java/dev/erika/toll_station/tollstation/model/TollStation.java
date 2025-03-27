c06698d247dc8252d1d84a3e687b48c5b79f1fa0
Erica <you@example.com>
2025-03-27 13:31:50 +0100

    feat: TollStation city implemented. TDD: ✔️

--- src/main/java/dev/erika/toll_station/tollstation/model/TollStation.java
+++ src/main/java/dev/erika/toll_station/tollstation/model/TollStation.java
@@ -3,12 +3,18 @@ package dev.erika.toll_station.tollstation.model;
 public class TollStation {
 
     private final String name;
+    private final String city;
 
-    public TollStation(String name) {
+    public TollStation(String name, String city) {
         this.name = name;
+        this.city = city;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
