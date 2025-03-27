9314304d0dbed0d035436b35491d419e8e3b8531
Erica <you@example.com>
2025-03-27 12:42:35 +0100

    feat: test PlateConstructor done. TDD: ❌

---
+++ src/main/java/dev/erika/toll_station/TollStationApplication.java
@@ -0,0 +1,12 @@
+package dev.erika.toll_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollStationApplication.class, args);
+	}
+}
