ef5a6242dba16dcb80c23de18fb1bbad3eac2aae
Erica <you@example.com>
2025-03-27 13:12:41 +0100

    feat: create TruckTest and constructor test done. TDD: ❌

---
+++ src/test/java/dev/erika/toll_station/vehicles/model/TruckTest.java
@@ -0,0 +1,27 @@
+package dev.erika.toll_station.vehicles.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TruckTest {
+    public class TestTruck extends Vehicle {
+        public TestTruck(String plate, int axles) {
+            super(plate);
+        }
+
+        int axles;
+
+        @Override
+        public int calculateToll() {
+            return 50*axles;
+        }
+    }
+
+    @Test
+    @DisplayName("Test para comprobar que Truck se extiende y se establece correctamente")
+    public void testTruckConstructor() {
+        Truck truck = new Truck("1234ABC", 2);
+        assertEquals("1234ABC", truck.getPlate());
+    }
+}
