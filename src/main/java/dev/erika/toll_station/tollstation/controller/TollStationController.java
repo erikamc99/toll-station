4fbdb957d8651c9ae2362252b90b2ebea2d7a7e8
Erica <you@example.com>
2025-03-27 17:16:09 +0100

    feat: method printReport implemented. TDD: ✔️

--- src/main/java/dev/erika/toll_station/tollstation/controller/TollStationController.java
+++ src/main/java/dev/erika/toll_station/tollstation/controller/TollStationController.java
@@ -1,7 +1,7 @@
 package dev.erika.toll_station.tollstation.controller;
 
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,5 +24,24 @@ public class TollStationController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<String> printReport() {
+        StringBuilder report = new StringBuilder();
+        report.append("=== Reporte de: ")
+        .append(tollStation.getName()).append(", ")
+        .append(tollStation.getCity()).append(" ===\n");
+        report.append("Vehículos atendidos:\n");
+
+        for (Vehicle v : tollStation.getVehicles()) {
+            report.append("Matrícula: ").append(v.getPlate())
+                  .append(", Peaje: ").append(v.calculateToll()).append("€\n");
+        }
+
+        int totalToll = tollStation.getVehicles().stream().mapToInt(Vehicle::calculateToll).sum();
+        report.append("\nTotal recolectado: ").append(totalToll).append("€");
+
+        return ResponseEntity.ok(report.toString());
     }
 }
