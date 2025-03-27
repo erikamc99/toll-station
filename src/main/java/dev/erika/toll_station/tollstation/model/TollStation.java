02be8a23920a4da9cac2b7b7c20210b5c486286e
Erica <you@example.com>
2025-03-27 13:28:59 +0100

    feat: TollSation created (only with name). TDD: ✔️

---
+++ src/main/java/dev/erika/toll_station/tollstation/model/TollStation.java
@@ -0,0 +1,14 @@
+package dev.erika.toll_station.tollstation.model;
+
+public class TollStation {
+
+    private final String name;
+
+    public TollStation(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
