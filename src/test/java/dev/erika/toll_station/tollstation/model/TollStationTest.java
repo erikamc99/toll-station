1c464ee8c4f976f0ea113cf1272f51a3527ddf7d
Erica <you@example.com>
2025-03-27 15:56:00 +0100

    feat: refactor TollStationTest to add the creation of a car at BeforeEach. TDD: ✔️

--- src/test/java/dev/erika/toll_station/tollstation/model/TollStationTest.java
+++ src/test/java/dev/erika/toll_station/tollstation/model/TollStationTest.java
@@ -12,10 +12,13 @@ import dev.erika.toll_station.vehicles.model.Car;
 public class TollStationTest {
 
     private TollStation tollStation;
+    private Car car;
 
     @BeforeEach
     void setUp() {
         tollStation = new TollStation("Autogrill", "Sagunto");
+        car = new Car("1234ABC");
+        tollStation.addVehicle(car);
     }
 
     @Test
@@ -28,22 +31,18 @@ public class TollStationTest {
     @Test
     @DisplayName("Test para comprobar que se crea la lista de vehículos")
     public void testVehicleList() {
-        assertEquals(0, tollStation.getVehicles().size());
+        assertEquals(1, tollStation.getVehicles().size());
     }
 
     @Test
     @DisplayName("Test para comprobar que se añade un coche a la lista")
     public void testAddVehicle() {
-        Car car = new Car("1234ABC");
-        tollStation.addVehicle(car);
         assertTrue(tollStation.getVehicles().contains(car));
     }
 
     @Test
     @DisplayName("Test para comprobar el cobro del peaje")
     public void testCollectToll() {
-        Car car = new Car("1234ABC");
-        tollStation.addVehicle(car);
         int toll = tollStation.collectToll(car);
         assertEquals(100, toll);
     }
