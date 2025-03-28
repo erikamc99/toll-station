e5a512f19a938e4a7aa7e5ef86823814fe3f51b8
Erica <you@example.com>
2025-03-28 09:19:17 +0100

    feat: main chaned to print a report. without test.

--- src/main/java/dev/erika/toll_station/TollStationApplication.java
+++ src/main/java/dev/erika/toll_station/TollStationApplication.java
@@ -1,12 +1,28 @@
 package dev.erika.toll_station;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import dev.erika.toll_station.tollstation.controller.TollStationController;
+import dev.erika.toll_station.tollstation.model.TollStation;
+import dev.erika.toll_station.vehicles.model.Car;
+import dev.erika.toll_station.vehicles.model.Motorbike;
+import dev.erika.toll_station.vehicles.model.Truck;
 
 @SpringBootApplication
 public class TollStationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TollStationApplication.class, args);
+		TollStation tollStation = new TollStation("Autogrill", "Sagunto");
+		TollStationController controller = new TollStationController(tollStation);
+		Car car = new Car("1234ABC");
+		Motorbike moto = new Motorbike("5678DEF");
+		Truck truck = new Truck("1234DEF", (int) (Math.random() * 10) + 1);
+		Truck truck2 = new Truck("1234GHI", (int) (Math.random() * 10) + 1);
+
+		controller.collectToll(car);
+		controller.collectToll(moto);
+		controller.collectToll(truck);
+		controller.collectToll(truck2);
+		System.out.println(controller.printReport());
 	}
 }
